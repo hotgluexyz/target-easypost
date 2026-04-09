b8898102f77f9b2109e6434f13d8f4d3d7dec221
Key <keynarafaelp@gmail.com>
2026-04-09 09:58:58 -0400

    fix shipment sink

--- target_easypost/sinks.py
+++ target_easypost/sinks.py
@@ -7,15 +7,13 @@ class ShipmentSink(EasypostStream):
     endpoint = "/shipments"
 
     def upsert_record(self, record: dict, context: dict):
-        record_id = record.pop("id", None)
-        is_update = record_id is not None
-
         endpoint = f"/{self.name}"
         method = "POST"
 
         response = self.request_api(method, endpoint, request_data=record)
         res_json = response.json()
         id = res_json.get("id")
+        state_updates = {}
 
         # buy a label
         # choose first option for now
@@ -25,10 +23,6 @@ class ShipmentSink(EasypostStream):
         first_rate = rates[0]
         buy_label_payload = {"rate": {"id": first_rate.get("id")}}
         self.request_api("POST", f"{self.endpoint}/{id}/buy", request_data=buy_label_payload)
-
-        state_updates = {}
-        if is_update and response.ok:
-            state_updates = {"is_updated": True}
 
         return id, response.ok, state_updates
     
