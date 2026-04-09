70d9178a3101951a2db13d3972dfc7115b9510a0
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2026-04-09 10:58:57 -0400

    HGI-10097: Fix shipment sink (#1)

--- target_easypost/sinks.py
+++ target_easypost/sinks.py
@@ -7,15 +7,16 @@ class ShipmentSink(EasypostStream):
     endpoint = "/shipments"
 
     def upsert_record(self, record: dict, context: dict):
-        record_id = record.pop("id", None)
-        is_update = record_id is not None
-
         endpoint = f"/{self.name}"
         method = "POST"
+        id = record.get("id")
+        if id:
+            raise ValueError("Shipment doesn't allow updates, remove id from record to create a new shipment")
 
         response = self.request_api(method, endpoint, request_data=record)
         res_json = response.json()
         id = res_json.get("id")
+        state_updates = {}
 
         # buy a label
         # choose first option for now
@@ -25,10 +26,6 @@ class ShipmentSink(EasypostStream):
         first_rate = rates[0]
         buy_label_payload = {"rate": {"id": first_rate.get("id")}}
         self.request_api("POST", f"{self.endpoint}/{id}/buy", request_data=buy_label_payload)
-
-        state_updates = {}
-        if is_update and response.ok:
-            state_updates = {"is_updated": True}
 
         return id, response.ok, state_updates
     
